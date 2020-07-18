ef2dc56f1df78f41449e4aab719d241d399779f1
DESKTOP-LBOHS1J\hungnm2 <hung.nguyen@netvisiontel.com>
2020-07-18 15:12:55 +0700

    update

--- basic-app/src/main/java/hung/com/test/App1_spi.java
+++ basic-app/src/main/java/hung/com/test/App1_spi.java
@@ -6,18 +6,23 @@ import java.util.ServiceLoader;
 
 import com.niaa.service.api.account.IAccount;
 
+
 public class App1_spi {
 
 	public static void main(String[] args) throws Exception {
 
-			// use Java Reflection to load all implementations of API
-		    // factory pattern to create object (new implementationClass )
-		    ServiceLoader<IAccount> loader = ServiceLoader.load(IAccount.class);
+			/**
+			 *  use Java Reflection to load all implementations of API
+			 *  factory pattern to create object (new implementationClass )
+			 */
+		    ServiceLoader<IAccount> serviceLoader = ServiceLoader.load(IAccount.class); 
 			
 			// iterator la kieu list of new ImplementationClass
-			Iterator<IAccount> iterator = loader.iterator();
+			Iterator<IAccount> iterator = serviceLoader.iterator();
 			
 			int i = 1;
+			// cách 1:
+			System.out.println("================================================= cách 1:");
 			while(iterator.hasNext()) {
 				System.out.println("======== implementation class: "+ i);
 				IAccount next = iterator.next();
@@ -25,6 +30,23 @@ public class App1_spi {
 				System.out.println("AccountId: "+ next.getAccountId());
 				
 			}
+			
+			// cách 2:
+			i = 1;
+			System.out.println("================================================= cách 2:");
+			for (IAccount account : serviceLoader) {
+		    	System.out.println("======== implementation class: "+ i++);
+		    	System.out.println("AccountType: "+ account.getAccountType());
+				System.out.println("AccountId: "+ account.getAccountId());
+		    }
+			
+			// cách 3 (nên dùng): lambda java 8
+			System.out.println("================================================= cách 3:");
+			serviceLoader.forEach(account->{
+				System.out.println("======== implementation class: ");
+		    	System.out.println("AccountType: "+ account.getAccountType());
+				System.out.println("AccountId: "+ account.getAccountId());
+			});
 
 	}
 
